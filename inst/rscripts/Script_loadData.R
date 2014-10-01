d50103616026607173f1e979c4a5069b9faa187f
welch16 <welch@stat.wisc.edu>
2014-10-01 17:49:07 -0500

    Add qname to GAlignments data

--- inst/rscripts/Script_loadData.R
+++ inst/rscripts/Script_loadData.R
@@ -16,18 +16,16 @@ names(files) = folder
 ff = lapply(folder,function(x,dr,files)file.path(dr,x,files[[x]]),dr,files)
 names(ff) = folder
 
-exo = mclapply(ff[[1]],FUN = readGAlignmentsFromBam,param = NULL,mc.cores = 8)
+set_param = ScanBamParam(what = "qname")
 
-set = mclapply(ff[[3]],FUN = readGAlignmentsFromBam,param = NULL,mc.cores = 8)
+exo = mclapply(ff[[1]],FUN = readGAlignmentsFromBam,param = set_param,mc.cores = 8)
+set = mclapply(ff[[3]],FUN = readGAlignmentsFromBam,param = set_param,mc.cores = 8)
+
 # This part is to distinguish the files as Paired End tags
+pet_flag = scanBamFlag(isPaired = TRUE, isProperPair = TRUE, isUnmappedQuery = NA,hasUnmappedMate = NA, isMinusStrand = NA, isMateMinusStrand = NA,isFirstMateRead = NA, isSecondMateRead = NA, isNotPrimaryRead = NA, isNotPassingQualityControls = NA, isDuplicate = NA)
 
-pet_flag = scanBamFlag(isPaired = TRUE, isProperPair = TRUE, isUnmappedQuery = NA,
-                hasUnmappedMate = NA, isMinusStrand = NA, isMateMinusStrand = NA,
-                isFirstMateRead = NA, isSecondMateRead = NA, isNotPrimaryRead = NA,
-                isNotPassingQualityControls = NA, isDuplicate = NA)
-pet_param = ScanBamParam(flag = pet_flag, simpleCigar = FALSE,
-                 reverseComplement = FALSE, tag = character(0),
-                 what = character(0))
+pet_flag = scanBamFlag(isPaired=TRUE,isProperPair=TRUE)
+pet_param = ScanBamParam(flag = pet_flag,what = "qname")
 pet = mclapply(ff[[2]][-c(11,12)],FUN = readGAlignmentsFromBam,param = pet_param,mc.cores = 8)
 
 names(exo) = sub("_qc.sorted.bam","",files[[1]])
