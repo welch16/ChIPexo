a60b3a7fc10d2ee57b7204cb70fc594de6b554dd
welch16 <welch@stat.wisc.edu>
2014-11-06 12:53:23 -0600

    Fixed separate reads functions, added messages and fixed typo where the data set with lower depth was used to build regions

--- inst/rscripts/Script_cluster_structure_slice.R
+++ inst/rscripts/Script_cluster_structure_slice.R
@@ -47,7 +47,7 @@ exo.depths = sapply(exo.sets,function(x)c(rep1=length(x[[1]]),rep2=length(x[[2]]
 pet.depths = sapply(pet.sets,function(x)c(rep1=length(x[[1]]),rep2=length(x[[2]])))
 set.depths = sapply(set.sets,function(x)c(rep1=length(x[[1]]),rep2=length(x[[2]])))
 
-max.depth.idx = ifelse(exo.depths[1,] > exo.depths[2,],2,1)
+max.depth.idx = ifelse(exo.depths[1,] > exo.depths[2,],1,2)
 exo.sets.use = mapply(function(x,y) x[[y]],exo.sets,max.depth.idx,SIMPLIFY =FALSE)
 
 fwd_cover = mclapply(exo.sets.use,function(x)coverage(subset(x,strand(x)=="+")))
@@ -71,7 +71,6 @@ fwd_regions = mclapply(fwd_regions,function(x)GRanges(seqnames ="U00096",ranges 
 bwd_regions = mclapply(bwd_regions,function(x)GRanges(seqnames ="U00096",ranges =x ,strand = "*"),mc.cores=4)
 
 minW = 0
-
 create_regions <- function(fwd,bwd,minW =0)
 {
   fwd_l = length(fwd)
@@ -118,8 +117,9 @@ dev.off()
 separated_reads<- function(reads,regions)
 {
   overlaps = findOverlaps(regions,reads)
-  n_regions = length(regions)
-  separated_reads = mclapply(1:n_regions,function(i,overlaps,reads)reads[subjectHits(overlaps[queryHits(overlaps)==i])],overlaps,reads,mc.cores = 8)
+  sep_index = split(overlaps,factor(queryHits(overlaps)))
+  separated_reads <- mclapply(sep_index,
+    function(x,reads)reads[subjectHits(x)],reads,mc.cores = 8)
   return(separated_reads)
 }
 
@@ -128,16 +128,24 @@ rif = c(0,0,20,20)
 files = paste0(ip,"_",rif,"min")
 ext= 100
 
+save(list = "ourRegions", file = file.path(datadir,"regions_slice.RData"))
+
+
 for(i in 1:4){
   print(i)
   regions = ourRegions[[i]]
+  print("Separating exo reads into regions")
   exo_sep_reads1 = separated_reads(exo.sets[[i]][[1]],regions)
   exo_sep_reads2 = separated_reads(exo.sets[[i]][[2]],regions)
+  print("Separating pet reads into regions")
   pet_sep_reads1 = separated_reads(pet.sets[[i]][[1]],regions)
   pet_sep_reads2 = separated_reads(pet.sets[[i]][[2]],regions)
+  print("Separating set reads into regions")
   set_sep_reads1 = separated_reads(set.sets[[i]][[1]],regions)
   set_sep_reads2 = separated_reads(set.sets[[i]][[2]],regions)
 
+  # note the list have different lengths
+  
   save(list = c("exo_sep_reads1","exo_sep_reads2","pet_sep_reads1",
          "pet_sep_reads2","set_sep_reads1","set_sep_reads2"),
        file = file.path(datadir,paste0(files[i],"_sep_reads.RData")))
@@ -145,13 +153,17 @@ for(i in 1:4){
   lowerBounds = start(regions)
   upperBounds = end(regions)
 
+  names(upperBounds) = as.character(1:length(regions))
+  names(lowerBounds) = as.character(1:length(regions))
+  
   pdf(file = file.path(figsdir,paste0(files[i],"_sliceRegions.pdf")),width =12,height =6)
-  mapply(all_plots2,lowerBounds,upperBounds,exo_sep_reads1,
-             exo_sep_reads2,pet_sep_reads1,
-             pet_sep_reads2,set_sep_reads1,
-             set_sep_reads2,ext)
+  lapply(as.character(1:length(regions)),function(j){
+    all_plots2(lowerBounds[j],upperBounds[j],
+       exo_sep_reads1[[j]],exo_sep_reads2[[j]],
+       pet_sep_reads1[[j]],pet_sep_reads2[[j]],
+       set_sep_reads1[[j]],set_sep_reads2[[j]],ext)
+  })
   dev.off()
 
 }
 
-save(list = "ourRegions", file = file.path(datadir,"regions_slice.RData"))
