f3b51b66628c4779a96be6e053fa4beafafc1a49
welch16 <welch@stat.wisc.edu>
2014-11-10 15:12:20 -0600

    Added fix when there is not reads for island

--- inst/rscripts/Script_slice_summary_stat.R
+++ inst/rscripts/Script_slice_summary_stat.R
@@ -48,28 +48,33 @@ if(regionsFile == "Sig70_0min_sep_reads.RData"){
   q()}
 
 library(GenomicAlignments)
-
-
-
 load(file.path(datadir,regionsFile))
 
 diff_pos_summit <- function(reads)
-{
-  fwd_cover = suppressWarnings(coverage(subset(reads,strand(reads)=="+"))[[1]])
-  bwd_cover = suppressWarnings(coverage(subset(reads,strand(reads)=="-"))[[1]])
-  max_fwd = NA
-  fwd_summit_pos = NA
-  if(nrun(fwd_cover) > 1){
-    max_fwd = max(fwd_cover)
-    fwd_summit_pos = head(which(fwd_cover == max_fwd),n=1)
+{  
+  if(!is.null(reads)){
+    fwd_cover = suppressWarnings(coverage(subset(reads,strand(reads)=="+"))[[1]])
+    bwd_cover = suppressWarnings(coverage(subset(reads,strand(reads)=="-"))[[1]])
+    max_fwd = NA
+    fwd_summit_pos = NA
+    if(nrun(fwd_cover) > 1){
+      max_fwd = max(fwd_cover)
+      fwd_summit_pos = head(which(fwd_cover == max_fwd),n=1)
+    }
+    max_bwd = NA
+    bwd_summit_pos = NA
+    if(nrun(bwd_cover) > 1){
+      max_bwd = max(bwd_cover)
+      bwd_summit_pos = tail(which(bwd_cover == max_bwd),n=1)
+    }
+    diff = bwd_summit_pos - fwd_summit_pos    
+  }else{    
+    diff = NA
+    max_fwd = NA
+    max_bwd = NA
+    fwd_summit_pos = NA
+    bwd_summit_pos = NA
   }
-  max_bwd = NA
-  bwd_summit_pos = NA
-  if(nrun(bwd_cover) > 1){
-    max_bwd = max(bwd_cover)
-    bwd_summit_pos = tail(which(bwd_cover == max_bwd),n=1)
-  }  
-  diff = bwd_summit_pos - fwd_summit_pos    
   return(c(diff=diff,max_fwd=max_fwd,max_bwd=max_bwd,fwd_pos = fwd_summit_pos,bwd_pos = bwd_summit_pos))
 }
 
@@ -89,8 +94,8 @@ library(parallel)
 
 summary_statistics <- function(reads,regions,mc)
 {
-  fwd_strand = t(do.call(cbind,mclapply(1:length(regions),function(i,reads)fwd_strand_ratio(reads[[i]]),reads,mc.cores = mc)))
-  dif_pos = t(do.call(cbind,mclapply(1:length(regions),function(i)diff_pos_summit(reads[[i]]),mc.cores = mc)))
+  fwd_strand = t(do.call(cbind,mclapply(as.character(1:length(regions)),function(i,reads)fwd_strand_ratio(reads[[i]]),reads,mc.cores = mc)))
+  dif_pos = t(do.call(cbind,mclapply(as.character(1:length(regions)),function(i)diff_pos_summit(reads[[i]]),mc.cores = mc)))
   df = DataFrame(cbind(fwd_strand,dif_pos))
   return(df)
 }
@@ -108,6 +113,6 @@ set1 = summary_statistics(set_sep_reads1,regions,8)
 # Set2
 set2 = summary_statistics(set_sep_reads2,regions,8)
 
-save(file = file.path(datadir,regionsFile),list = c("regions","exo1","exo2","pet1","pet2","set1","set2"))
+save(file = file.path(datadir,paste0("output",regionsFile)),list = c("regions","exo1","exo2","pet1","pet2","set1","set2"))
   
 q()
