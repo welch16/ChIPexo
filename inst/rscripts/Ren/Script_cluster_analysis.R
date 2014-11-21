7ad82eb850a6ad508a1ac9c6c16a71843fb77155
welch16 <welch@stat.wisc.edu>
2014-11-21 13:36:03 -0600

    Adapted code to decrease subsetting time

--- inst/rscripts/Ren/Script_cluster_analysis.R
+++ inst/rscripts/Ren/Script_cluster_analysis.R
@@ -5,9 +5,8 @@ library(ggplot2)
 library(GenomicAlignments)
 library(parallel)
 library(knitr)
-library(wavelets)
-library(RColorBrewer)
-library(scales)
+#library(RColorBrewer)
+#library(scales)
 
 datadir = "data"
 load(file = file.path(datadir,"Ren_reads.RData"))
@@ -19,7 +18,6 @@ reads_bwd = mclapply(reads,function(x)subset(x,strand(x)=="-"),mc.cores= 6)
 
 cover_fwd = mclapply(reads_fwd,coverage,mc.cores = 6)
 cover_bwd = mclapply(reads_bwd,coverage,mc.cores = 6)
-
 
 slice_all <- function(cover,lw)
 {
@@ -48,17 +46,56 @@ create_regions <- function(fwd,bwd,minW =0)
   return(out)
 }
 
-ourRegions = mcmapply(create_regions,regions_fwd,regions_bwd,MoreArgs = list(minW=50),SIMPLIFY=FALSE,mc.cores =6)
+ourRegions = mcmapply(create_regions,regions_fwd,regions_bwd,MoreArgs = list(minW=0),SIMPLIFY=FALSE,mc.cores =6)
+chr = paste0("chr",c(1:22,"X","Y"))
+
+separate_regions <- function(regions,chr)
+{
+  regions = mclapply(chr,function(x,regions){
+    u = subset(regions,seqnames == x)
+    names(u) = NULL
+    return(u)},regions,mc.cores =8)
+  regions = GRangesList(regions)
+  return(regions)
+}
+
+ourRegions = lapply(ourRegions,separate_regions,chr)
+ourReads = lapply(reads,separate_regions,chr)
 
 
-separated_reads<- function(reads,regions)
+## dr = "../RenData/BAMfiles"
+
+
+## # AY552.R1.
+## param = ScanBamParam(which = ourRegions[[1]])
+## reads = readGAlignmentsFromBam(file.path(dr,names(ourRegions)[1]),param = param,use.names = FALSE)
+
+## save(list = "ourRegions",file = file.path(datadir,"Ren_regions.RData"))
+
+## load(file =file.path(datadir,paste0("Ren_",names(ourRegions)[1],"_sepReads.RData")))
+
+
+separate_reads<- function(reads,regions)
 {
   overlaps = findOverlaps(regions,reads)
-  n_regions = length(regions)
-  separated_reads = mclapply(1:n_regions,function(i,overlaps,reads)reads[subjectHits(overlaps[queryHits(overlaps)==i])],overlaps,reads,mc.cores = 8)
+  sep_index = split(overlaps,factor(queryHits(overlaps)))
+  separated_reads <- mclapply(sep_index,
+    function(x,reads)reads[subjectHits(x)],reads,mc.cores = 8)
   return(separated_reads)
 }
 
-sep_reads = mapply(separated_reads,reads,ourRegions,SIMPLIFY = FALSE)
 
-save(file = file.path(datadir,"Ren_separated_reads.RData"),list = "sep_reads")
+
+ourReads_sep = list()
+j=5
+  print(j)
+  ourReads_sep[[j]] = mapply(separate_reads,ourReads[[j]],ourRegions[[j]],SIMPLIFY = FALSE)
+  sep_reads = ourReads_sep[[j]]
+  reg = ourRegions[[j]]
+  save(list = c("sep_reads","reg"),file =file.path(datadir,paste0("Ren_",names(ourReads)[j],"_sepReads.RData")))
+
+
+## names(ourReads_sep) = names(ourReads)
+## save(file = file.path(datadir,"Ren_separated_reads.RData"),list = c("ourReads_sep","ourRegions"))
+
+
