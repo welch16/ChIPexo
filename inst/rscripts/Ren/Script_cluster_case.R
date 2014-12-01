bdd7eaf6f66c74ad9138480da7240470d5ff9f5e
welch16 <welch@stat.wisc.edu>
2014-12-01 01:01:13 -0600

    Fixed to calculate summary stats with data.table

---
+++ inst/rscripts/Ren/Script_cluster_case.R
@@ -0,0 +1,211 @@
+
+rm(list = ls())
+library(GenomicAlignments)
+library(parallel)
+library(data.table)
+library(pryr)
+
+dr = "/p/keles/ChIPexo/volume3/ChIPexo/data/Ren/separated"
+
+load(file = file.path(dr,"separated_reads.RData"))
+# reads
+load(file = file.path(dr,"separated_regions.RData"))
+# regions
+load(file = file.path(dr,"separated_overlaps.RData"))
+# overlaps
+
+# Want to for each data set obtain a set of summary statistics
+
+sets = names(overlaps)
+labels = names(overlaps[[1]])
+strands = names(overlaps[[1]][[1]])
+chr = names(overlaps[[1]][[1]][[1]])
+
+extract_reads <- function(param,reads,regions,overlaps)
+{
+  ourReads = reads[[param[3]]][[param[1]]][[param[4]]]
+  ourRegions = regions[[param[2]]][[param[1]]][[param[4]]]
+  ourOverlaps = overlaps[[param[1]]][[param[2]]][[param[3]]][[param[4]]]
+  ourReads = data.table(as.data.frame(ourReads))
+  ourReads$ID = 1:nrow(ourReads)
+  ourOverlaps = data.table(as.data.frame(ourOverlaps))
+  setkey(ourOverlaps,queryHits)
+  setkey(ourReads,ID)
+  ourReads = ourReads[ourOverlaps[queryHits %in% 1:length(ourRegions),list(subjectHits)],]
+  ourReads$region = ourOverlaps[,list(queryHits)]
+  return(ourReads)
+}
+
+
+build_param_Df <- function(set,labels,strands,chr)
+{
+  paramDf = as.data.frame(matrix(nrow = length(labels)
+    *length(strands)*length(chr),ncol=4))
+  names(paramDf) = c("set","label","strand","chr")
+  k=1
+  for(lab in labels){
+    for(stran in strands){
+      for(ch in chr){
+        paramDf[k,] = c(set,lab,stran,ch)
+        k = k+1
+      }
+    }
+  }
+  paramDf = data.table(paramDf)
+  paramDf$ID = 1:nrow(paramDf)
+  return(paramDf)
+}
+
+paramDf1 = build_param_Df(sets[1],labels,strands,chr)
+paramDf2 = build_param_Df(sets[2],labels,strands,chr)
+paramDf3 = build_param_Df(sets[3],labels,strands,chr)
+
+setkey(paramDf1,ID,label,strand,chr)
+setkey(paramDf2,ID,label,strand,chr)
+setkey(paramDf3,ID,label,strand,chr)
+
+
+sep_reads1 = mclapply(1:nrow(paramDf1),function(i){
+  param = as.character(paramDf1[ID==i,list(set,label,strand,chr)])
+  separated_reads<- extract_reads(param,reads,regions,overlaps)
+  return(separated_reads)},mc.cores = 12)
+
+sep_reads2 = mclapply(1:nrow(paramDf2),function(i){
+  param = as.character(paramDf2[ID==i,list(set,label,strand,chr)])
+  separated_reads<- extract_reads(param,reads,regions,overlaps)
+  return(separated_reads)},mc.cores = 12)
+
+sep_reads3 = mclapply(1:nrow(paramDf2),function(i){
+  param = as.character(paramDf3[ID==i,list(set,label,strand,chr)])
+  separated_reads<- extract_reads(param,reads,regions,overlaps)
+  return(separated_reads)},mc.cores = 12)
+
+
+data.table2IRanges <- function(DT){
+  ranges = IRanges(start = DT$start,
+      end = DT$end)
+  return(ranges)
+}
+
+data.table2GRanges <- function(DT){
+  sn = as.character(DT$seqnames)
+  gr = GRanges(seqnames = sn,
+    ranges = data.table2IRanges(DT),
+    strand = DT$strand)
+  return(gr)
+}
+
+
+strand_statistics <- function(region_reads,param,idx)
+{
+  # depth
+  depth = nrow(region_reads)
+  # Nr. positions
+  ranges= data.table2IRanges(region_reads)
+  if(as.character(param[,list(strand)])=="fwd"){
+    nrPos = length(unique(start(ranges)))
+  }else{
+    nrPos = length(unique(end(ranges)))
+  }
+  # Summit position
+  cover = coverage(ranges)
+  if(nrun(cover) > 1){
+    maxCover = max(cover)
+    if(as.character(param[,list(strand)])=="fwd"){
+      summitPos = head(which(cover == maxCover),n=1)
+    }else{
+      summitPos = tail(which(cover == maxCover),n=1)
+    }
+  }else{
+    maxCover = NA
+    summitPos = NA
+  }
+  return(c(idx=idx,depth=depth,nrPos =nrPos,maxCover=maxCover,summitPos=summitPos))
+}
+
+summaryStats1 = lapply(1:nrow(paramDf1),function(j){
+  print(paste0(j,"/144"))
+  reg = as.numeric(unique(sep_reads1[[j]][,list(region)])[[1]])
+  stats = mclapply(reg,
+    function(i)strand_statistics(sep_reads1[[j]][region==i,],paramDf1[ID==i,],i),mc.cores=12)
+  return(stats)
+})
+
+summaryStats2 = lapply(1:nrow(paramDf2),function(j){
+  print(paste0(j,"/144"))
+  reg = as.numeric(unique(sep_reads2[[j]][,list(region)])[[1]])
+  stats = mclapply(reg,
+    function(i)strand_statistics(sep_reads2[[j]][region==i,],paramDf2[ID==i,],i),mc.cores=12)
+  return(stats)
+})
+
+
+summaryStats3 = lapply(1:nrow(paramDf3),function(j){
+  print(paste0(j,"/144"))  
+  reg = as.numeric(unique(sep_reads3[[j]][,list(region)])[[1]])
+  stats = mclapply(reg,
+    function(i)strand_statistics(sep_reads3[[j]][region==i,],paramDf3[ID==i,],i),mc.cores=12)
+  return(stats)
+})
+
+save(list = "summaryStats1",file = file.path(dr,"AY552_summary.RData"))
+save(list = "summaryStats2",file = file.path(dr,"AY553_summary.RData"))
+save(list = "summaryStats3",file = file.path(dr,"AY554_summary.RData"))
+
+
+summary_statistics <- function(reads)
+{
+  reads = data.table(as.data.frame(reads))
+  setkey(reads,strand)
+  fwd = na.omit(reads["+"])
+  bwd = na.omit(reads["-"])
+  f = nrow(fwd)
+  r = nrow(bwd)
+  
+  f_pos = length(unique(fwd$start))
+  b_pos = length(unique(bwd$end))
+
+  max_fwd = NA
+  fwd_summit_pos = NA
+  
+  if( f > 0){
+    fwd = GRanges(fwd$seqnames,ranges = IRanges(start = fwd$start,end=fwd$end),strand = "+")
+    fwd_cover = coverage(fwd)[[1]]
+    if(nrun(fwd_cover) > 1){
+      max_fwd = max(fwd_cover)
+      fwd_summit_pos = head(which(fwd_cover == max_fwd),n=1)
+    }
+  }
+
+  max_bwd = NA
+  bwd_summit_pos = NA
+  if(r > 0){  
+    bwd = GRanges(bwd$seqnames,ranges = IRanges(start = bwd$start,end=bwd$end),strand = "-")
+    bwd_cover = coverage(bwd)[[1]]
+    if(nrun(bwd_cover) > 1){
+      max_bwd = max(bwd_cover)
+      bwd_summit_pos = tail(which(bwd_cover == max_bwd),n=1)
+    }
+  }
+  depth = f + r
+  prob = f / depth
+  nrPos = f_pos + b_pos
+  
+  diff = bwd_summit_pos - fwd_summit_pos    
+  out = c(f=f,r=r,prob=prob,depth=depth,f_uniq = f_pos,r_uniq = b_pos,n_uniq = nrPos,max_f = max_fwd,
+    max_r = max_bwd,max_f_pos = fwd_summit_pos,max_r_pos = bwd_summit_pos,diff = diff)
+  return(out)
+    
+}
+
+both_idx = lapply(reg,function(x)as.character(which(x$label == "both")))
+
+sub_sep_reads_idx = mapply(function(x,y){
+  return(which(names(x) %in% y))},sep_reads,both_idx,SIMPLIFY=FALSE)
+
+sub_sep_reads = mapply(function(x,y){
+  return(x[y])},sep_reads,sub_sep_reads_idx,SIMPLIFY=FALSE)
+
+summary_stats = lapply(sub_sep_reads,function(x) data.table(t(mcmapply(summary_statistics,x,mc.cores=8))))
+
+save(file = file.path(datadir,"Ren2_summary_both.RData"),list = "summary_stats")
