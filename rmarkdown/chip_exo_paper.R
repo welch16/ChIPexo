46e69a14c9582f0f9cc2cb8c81714913d14d6836
welch16 <welch@stat.wisc.edu>
2015-11-10 12:39:44 -0600

    Finished analysis of local SCC for Sig70 data

--- rmarkdown/chip_exo_paper.R
+++ rmarkdown/chip_exo_paper.R
@@ -1,3 +1,9 @@
+## ----comp_param,include = FALSE,echo = FALSE,eval = TRUE-----------------
+
+  bin_size <- 150
+  frag_len <- 150
+  
+
 ## ----design_param , include = FALSE , echo = FALSE , eval = TRUE---------
 
   K <- 10
