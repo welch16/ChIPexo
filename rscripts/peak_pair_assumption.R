29a2a6631c3baf3728a7f0b3fc708009a2bb19ce
welch16 <welch@stat.wisc.edu>
2015-11-12 16:16:08 -0600

    codes to asses peak pair assumption

---
+++ rscripts/peak_pair_assumption.R
@@ -0,0 +1,195 @@
+
+
+rm(list = ls())
+
+library(data.table)
+library(GenomicAlignments)
+library(ggplot2)
+library(MASS)
+library(RColorBrewer)
+library(ChIPUtils)
+
+######################################################################################
+
+## Initial parameters
+
+mc <- detectCores()
+figs_dir <- "figs/condition"
+
+base_dir <- "/p/keles/ChIPexo/volume6/condition"
+
+check_create <- function(dr)if(!dir.exists(dr))dir.create(dr)
+
+
+figs_dir <- file.path(figs_dir,"all_together")
+check_create(figs_dir)
+
+######################################################################################
+
+## get promoter locations table
+
+prom_dir <- "/p/keles/ChIPexo/volume6/gold_standard/Landick"
+pfiles <- list.files(prom_dir)
+pfiles <- pfiles[grep(".bed",pfiles)]
+promoters <- lapply(file.path(prom_dir,pfiles),read.table)
+promoters <- lapply(promoters,data.table)
+
+prom <- read.table(
+    "/NO_BACKUP/KelesGroup_DongjunChung/ChIP-exo/Landick_ChIP-exo/database_regulonDB/PromoterSigma70Set.txt",
+  skip=33, sep="\t")
+loc.prom <- prom[,4]
+
+
+
+promoters[[1]][,str := "R"]
+promoters[[2]][,str := "F"]
+
+sites <- do.call(rbind,promoters)
+sites[, id := 1:nrow(sites)]
+setnames(sites,names(sites),c("seqnames","start","end","strand","id"))
+
+sites[,seqnames := "U00096"]
+sites[,strand := ifelse(strand == "F","+","-")]
+
+zites <- data.table(seqnames = "U00096",start = loc.prom, end = loc.prom , strand = "*")
+
+## ######################################################################################
+
+out_dir <- "/p/keles/ChIPexo/volume6/peak_pair"
+bs_dir <- file.path(out_dir,"binding")
+check_create(bs_dir)
+
+  load(file = file.path(bs_dir, "dpeakFit_exo_sig70aerobic.RData"))
+  load( file = file.path(bs_dir, "dpeakFit_pet1_sig70aerobic.RData"))
+  load( file = file.path(bs_dir, "dpeakFit_set_sig70aerobic.RData"))
+
+fit_comparison <- function(fit1 , fit2 , mm)
+{
+  mu1 <- lapply(fit1@optFit,function(x)x$mu)
+  mu2 <- lapply(fit2@optFit,function(x)x$mu)
+
+  G1 <- sapply(mu1,length)
+  G2 <- sapply(mu2,length)
+
+  print(identical(mu1,mu2))
+  print(table(G1))
+  print(table(G2))
+  print(table(G1 , G2))
+
+  min_dist <- function(m1,m2){
+
+    out <- sapply(m1 , function(x) min(abs(x - m2)))
+    out <- min(out)
+
+    return(out)
+
+  }
+  
+  alls <- mapply(c , mu1 , mu2 ,SIMPLIFY = FALSE)
+  alls <- lapply(alls,sort)
+
+  alls <- lapply(alls,diff)
+
+  alls <- sapply(alls,mean)
+  
+  dt <- data.table( G1 , G2  , mean_dist = alls, min = mapply(min_dist,mu1,mu2))
+  dt[,diff := G1 - G2 ]
+
+  r <- viridis::viridis(100, option = "D")
+  
+  p1 <- ggplot(dt[,median(min),by = .(G1,G2)], aes(G1, G2 , fill = V1))+geom_tile()+
+    scale_fill_gradientn(colours = r,name = "")+ggtitle(mm)+xlab("common")+ylab("separate")+coord_flip()+
+    theme(legend.position = "top")
+  print(p1)
+  return(dt)
+
+}
+
+
+exo_out <- fit_comparison(exo_fits[[1]],exo_fits[[2]],"exo")
+pet_out <- fit_comparison(pet_fits1[[1]],pet_fits1[[2]],"pet")
+set_out <- fit_comparison(set_fits[[1]],set_fits[[2]],"set")
+dev.off()
+
+site_fit_match <- function(fit,sites,mm)
+{
+  sites <- dt2gr(sites)
+  end(sites) <- start(sites)
+  strand(sites) <- "*"
+  sites <- reduce(sites)
+
+  mu <- fit@optMu
+
+  peaks <- strsplit(names(mu),"_",fixed = TRUE)
+  peaks <- GRanges(seqnames = sapply(peaks,function(x)x[1]),
+                   ranges = IRanges(
+                     start = as.numeric(sapply(peaks,function(x)x[2])),
+                     end = as.numeric(sapply(peaks,function(x)x[3]))))
+
+  message(mm)
+  message("Nr. peaks: ",length(peaks))
+  
+  ov <- findOverlaps(peaks,sites)
+  ov <- data.table(as.data.frame(ov))
+  ov <- split(ov, ov[,(queryHits)])
+  
+  message("Nr. sites (low res): ",length(ov))
+  
+  out <- mclapply(names(ov),function(i,mu,sites,peaks){
+    est <- mu[as.numeric(i)][[1]]
+    names(est) <- NULL
+    truth <- start(sites[ov[[i]][,(subjectHits)]])
+    aux <- list()
+    aux[["peak"]] <- peaks[as.numeric(i)]
+    aux[["truth"]] <- truth
+    aux[["est"]] <- est
+    return(aux)},mu,sites,peaks,mc.cores = 16)
+
+  return(out)
+
+}
+
+exo_match1 <- site_fit_match(exo_fits[[1]],sites,"exo1")
+exo_match2 <- site_fit_match(exo_fits[[2]],sites,"exo2")
+pet_match <- site_fit_match(pet_fits1[[1]],sites,"pet")
+set_match1 <- site_fit_match(set_fits[[1]],sites,"set1")
+set_match2 <- site_fit_match(set_fits[[2]],sites,"set2")
+
+
+reso_table <- function(match,mm)
+{
+  reso <- lapply(match,function(x){
+    out <- sapply(x$truth,function(z)min(abs(z - x$est)))
+  return(out)})
+
+  reso <- unlist(reso)
+  out <- data.table(reso , seq = mm)
+
+  return(out)
+}
+
+reso1 <- do.call(rbind,list(
+  reso_table(exo_match1,"exo_common"),
+  reso_table(exo_match2,"exo_sep"),
+  reso_table(pet_match,"pet"),
+  reso_table(set_match1,"set_common"),
+  reso_table(set_match2,"set_sep")))
+
+
+
+exo_match1 <- site_fit_match(exo_fits[[1]],zites,"exo1")
+exo_match2 <- site_fit_match(exo_fits[[2]],zites,"exo2")
+pet_match <- site_fit_match(pet_fits1[[1]],zites,"pet")
+set_match1 <- site_fit_match(set_fits[[1]],zites,"set1")
+set_match2 <- site_fit_match(set_fits[[2]],zites,"set2")
+
+reso2 <- do.call(rbind,list(
+  reso_table(exo_match1,"exo_common"),
+  reso_table(exo_match2,"exo_sep"),
+  reso_table(pet_match,"pet"),
+  reso_table(set_match1,"set_common"),
+  reso_table(set_match2,"set_sep")))
+
+
+ggplot(reso[reso < 100],aes(seq,reso))+geom_boxplot()+ylim(0,75)
+dev.off()
---
+++ rscripts/peak_pair_assumption_new.R
@@ -0,0 +1,173 @@
+
+rm(list = ls())
+
+library(mosaics)
+library(dpeak)
+library(ChIPUtils)
+
+exo_file <- "/p/keles/ChIPexo/volume3/LandickData/ChIPexo/edsn931_042814_qc.sorted.bam"
+pet_file1 <- "/p/keles/ChIPexo/volume3/LandickData/ChIPseq_PET/edsn788_042814_qc.filter.bam"
+pet_file2 <- "/p/keles/ChIPexo/volume3/LandickData/ChIPseq_PET/edsn788_042814_qc.sorted.bam"
+set_file <- "/p/keles/ChIPexo/volume3/LandickData/ChIPseq_SET/edsn80_042814_qc.sorted.bam"
+
+frag_len <- 150
+bin_size <- 150
+
+extra <- list()
+extra[["exo"]] <- file.path("/p/keles/genome_data/EColi_U00096.2",
+            c("mappability",
+              "GC",
+              "N"),"bin",
+            c("fragL150_bin150_32mer_single/E.coli_K-12_MG1655_GENOME_fragL150_bin150.txt",
+              "fragL150_bin150/E.coli_K-12_MG1655_GENOME_GC_fragL150_bin150.txt",
+              "fragL150_bin150/E.coli_K-12_MG1655_GENOME_N_fragL150_bin150.txt"))
+extra[["pet"]] <- "/p/keles/ChIPexo/volume6/inputs/pet/edsn1369_042814_qc.filter.bam_bin150.txt"
+extra[["set"]] <- "/p/keles/ChIPexo/volume6/inputs/set/edsn101_042814_qc.sorted.bam_fragL150_bin150.txt"
+
+
+check_create <- function(dr)if(!dir.exists(dr))dir.create(dr)
+
+
+out_dir <- "/p/keles/ChIPexo/volume6/peak_pair"
+
+### create bins
+
+
+bin_flag <- FALSE
+
+bin_dir <- file.path(out_dir,"bins")
+check_create(bin_dir)
+
+
+if(bin_flag){
+  constructBins(exo_file,fileFormat = "bam",outfileLoc = bin_dir,PET = FALSE,
+                fragLen = frag_len,binSize = bin_size)
+  constructBins(pet_file1,fileFormat = "bam",outfileLoc = bin_dir, PET =  TRUE,
+               binSize = bin_size)
+  constructBins(pet_file2,fileFormat = "bam",outfileLoc = bin_dir, PET =  TRUE,
+               binSize = bin_size)
+  constructBins(set_file,fileFormat = "bam",outfileLoc = bin_dir, PET =  FALSE,
+               fragLen = frag_len , binSize = bin_size)  
+}
+
+### call peaks
+
+
+
+peak_flag <- TRUE
+
+if(peak_flag){
+  exo_bin <- file.path(bin_dir,paste0(basename(exo_file),"_fragL",frag_len,"_bin",bin_size,".txt"))
+  pet_bin1 <- file.path(bin_dir,paste0(basename(pet_file1),"_bin",bin_size,".txt"))
+  pet_bin2 <- file.path(bin_dir,paste0(basename(pet_file2),"_bin",bin_size,".txt"))
+  set_bin <- file.path(bin_dir,paste0(basename(set_file),"_fragL",frag_len,"_bin",bin_size,".txt"))
+  
+  exo_bins <- readBins(c("chip","M","GC","N"),c(exo_bin,extra[["exo"]]))
+  pet_bins1 <- readBins(c("chip","input"),c(pet_bin1,extra[["pet"]]))
+  pet_bins2 <- readBins(c("chip","input"),c(pet_bin2,extra[["pet"]]))
+  set_bins <- readBins(c("chip","input"),c(set_bin,extra[["set"]]))
+
+  
+  exo_fit <- mosaicsFit(exo_bins)
+  pet_fit1 <- mosaicsFit(pet_bins1)
+  pet_fit2 <- mosaicsFit(pet_bins2)
+  set_fit <- mosaicsFit(set_bins)
+
+  pdf(file = "figs/sig70/gof_plots_sig70.pdf")
+  plot(exo_fit)
+  plot(pet_fit1)
+  plot(pet_fit2)
+  plot(set_fit)
+  dev.off()
+
+
+  get_peaks <- function(fit,fdr,bin_size,thresh)
+  {
+    if(fit@bic2S <= fit@bic1S){
+      peak <- mosaicsPeak(fit , signalModel = "2S", FDR = fdr, maxgap = bin_size ,thres = thresh)
+    }else{
+      peak <- mosaicsPeak(fit , signalModel = "1S", FDR = fdr, maxgap = bin_size ,thres = thresh)
+    }
+    show(peak)
+    return(data.table(peak@peakList))
+  }
+  
+  fdr <- .05
+  thresh <- 10
+
+  exo_peak <- get_peaks(exo_fit,fdr,bin_size,thresh)
+  pet_peak1 <- get_peaks(pet_fit1,fdr,bin_size,thresh)
+  pet_peak2 <- get_peaks(pet_fit2,fdr,bin_size,thresh)
+  set_peak <- get_peaks(set_fit,fdr,bin_size,thresh)
+
+  peak_dir <- file.path(out_dir,"peaks")
+  check_create(peak_dir)
+  exo_peak_file <- file.path(peak_dir,gsub("_qc.sorted.bam","_peaks.txt",basename(exo_file)))
+  pet_peak_file1 <-  file.path(peak_dir,gsub("_qc.filter.bam", "_filter_peaks.txt",basename(pet_file1)))
+  pet_peak_file2 <- file.path(peak_dir, gsub("_qc.sorted.bam", "_all_peaks.txt",basename(pet_file2)))
+  set_peak_file <-  file.path(peak_dir,gsub("_qc.sorted.bam", "_peaks.txt",basename(set_file)))
+
+  
+
+  write.table(exo_peak,file = exo_peak_file, sep = "\t",row.names = FALSE,quote = FALSE,col.names=FALSE)
+  write.table(pet_peak1, file  = pet_peak_file1, sep = "\t",row.names = FALSE,quote = FALSE,col.names=FALSE)
+  write.table(pet_peak2, file  = pet_peak_file2, sep = "\t",row.names = FALSE,quote = FALSE,col.names=FALSE)
+  write.table(set_peak , file = set_peak_file, sep = "\t",row.names = FALSE,quote = FALSE,col.names=FALSE)
+}
+
+## estimate binding events
+
+bs_dir <- file.path(out_dir,"binding")
+check_create(bs_dir)
+
+bind_flag <- TRUE
+
+if(bind_flag){
+
+  ## exo_dp <- dpeakRead(peakfile = exo_peak_file,readfile = exo_file , fileFormat = "bam",
+  ##                 PET = FALSE, fragLen = frag_len,parallel = TRUE,nCore = 18)
+  ## exo_fits <- list()
+  ## exo_fits[["common"]] <- dpeakFit(exo_dp,nCore = 18) 
+  ## exo_fits[["separate"]] <- dpeakFit(exo_dp,estDeltaSigma = "separate",nCore = 18)
+  ## save( exo_fits, file = file.path(bs_dir, "dpeakFit_exo_sig70aerobic.RData"))
+
+  ## pet_dp1 <- dpeakRead(peakfile = pet_peak_file1,readfile = pet_file1 , fileFormat = "bam",
+  ##                 PET = TRUE, fragLen = frag_len,parallel = TRUE,nCore = 18)   
+  ## pet_fits1 <- list()
+  ## pet_fits1[["common"]] <- dpeakFit(pet_dp1,nCore = 18) 
+  ## pet_fits1[["separate"]] <- dpeakFit(pet_dp1,estDeltaSigma = "separate",nCore = 18)
+  ## save( pet_fits1, file = file.path(bs_dir, "dpeakFit_pet1_sig70aerobic.RData"))
+
+
+  pet_dp2 <- dpeakRead(peakfile = pet_peak_file2,readfile = pet_file2 , fileFormat = "bam",
+                  PET = TRUE, fragLen = frag_len,parallel = TRUE,nCore = 18)
+  pet_fits2 <- list()
+  pet_fits2[["common"]] <- dpeakFit(pet_dp2,nCore = 18) 
+  pet_fits2[["separate"]] <- dpeakFit(pet_dp2,estDeltaSigma = "separate",nCore = 18)
+  save( pet_fits2, file = file.path(bs_dir, "dpeakFit_pet2_sig70aerobic.RData"))
+  
+  ## set_fits <- list()
+  ## set_dp <- dpeakRead(peakfile = set_peak_file,readfile = set_file , fileFormat = "bam",
+  ##                 PET = FALSE, fragLen = frag_len,parallel = TRUE,nCore = 18)
+  ## set_fits[["common"]] <- dpeakFit(set_dp,nCore = 18) 
+  ## set_fits[["separate"]] <- dpeakFit(set_dp,estDeltaSigma = "separate",nCore = 18)    
+  ## save( set_fits, file = file.path(bs_dir, "dpeakFit_set_sig70aerobic.RData"))
+
+}
+
+## load( file = file.path(bs_dir, "dpeakFit_sig70aerobic.RData"))
+
+## sites1 <- data.table(read.table(file.path(bs_dir,"sig70_bs_common.bed"),skip = 1))
+## sites2 <- data.table(read.table(file.path(bs_dir,"sig70_bs_separate.bed"),skip = 1))
+
+## peaks[ , fsr := sapply(dp1@fragSet, function(x) mean(x[,3] == "F"))]
+
+
+## library(ggplot2)
+## library(scales)
+
+
+
+
+
+  
