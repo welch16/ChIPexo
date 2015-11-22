04705321a8ec2df209167cdd39426b6dc2574a73
welch16 <welch@stat.wisc.edu>
2015-11-22 00:45:17 -0600

    R code for paper, mostly parameters

---
+++ rmarkdown/prelims_paper.R
@@ -0,0 +1,17 @@
+## ----comp_param,include = FALSE,echo = FALSE,eval = TRUE-----------------
+
+  bin_size <- 150
+  frag_len <- 150
+  
+
+## ----reso_param, include = FALSE , eval  = TRUE , echo = FALSE-----------
+
+  ext <- 20
+
+
+## ----design_param , include = FALSE , echo = FALSE , eval = TRUE---------
+
+  K <- 10
+  ext <- 15
+  
+
