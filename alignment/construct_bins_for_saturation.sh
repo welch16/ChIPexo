ec4dc3d183696a1956da0fa4ebcefbae72e596d6
welch16 <welch@stat.wisc.edu>
2016-01-27 14:56:59 -0600

    separated pet and set parts from script

--- alignment/construct_bins_for_saturation.sh
+++ alignment/construct_bins_for_saturation.sh
@@ -12,16 +12,3 @@ indir1=$indir1/samples
 
 rscripts/construct_bins.R $indir1 $outdir1 $fragLen $binSize FALSE
 
-indir2=$indir/ChIPseq_PET/seed$seed
-outdir2=$indir2/bins
-mkdir $outdir2
-indir2=$indir2/samples
-
-rscripts/construct_bins.R $indir2 $outdir2 $fragLen $binSize TRUE
-
-indir3=$indir/ChIPseq_SET/seed$seed
-outdir3=$indir3/bins
-mkdir $outdir3
-indir3=$indir3/samples
-
-rscripts/construct_bins.R $indir3 $outdir3 $fragLen $binSize FALSE
