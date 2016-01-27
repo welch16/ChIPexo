b5dcc922f48b36a7eb6f2f38856c3b0d6ddf3cbc
welch16 <welch@stat.wisc.edu>
2016-01-27 15:02:08 -0600

    script that converts to bed from bam

---
+++ alignment/convert_to_bed.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+basedir=/p/keles/ChIPexo/volume7/Landick/ChIPexo
+
+indir=$basedir/rif_treatment
+outdir=$basedir/bedfiles
+
+bedtools bamtobed -i $indir/edsn1311_Sig70.sort.bam > $outdir/edsn1311_Sig70.sort.bed
+bedtools bamtobed -i $indir/edsn1314_Sig70.sort.bam > $outdir/edsn1314_Sig70.sort.bed
+bedtools bamtobed -i $indir/edsn1317_Sig70.sort.bam > $outdir/edsn1317_Sig70.sort.bed
+bedtools bamtobed -i $indir/edsn1320_Sig70.sort.bam > $outdir/edsn1320_Sig70.sort.bed
+
