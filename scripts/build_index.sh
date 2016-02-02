ec1d282cf6a326afed5e4f178dee2452a0a17f76
welch16 <welch@stat.wisc.edu>
2016-02-01 20:26:40 -0600

    changed to K12 instead of RL3000

--- scripts/build_index.sh
+++ scripts/build_index.sh
@@ -3,6 +3,6 @@
 # script that builds the index to use for Bob Landick's ChIP-exo data
 
 dr=/p/keles/ChIPexo/volume7/Landick/index
-file=RL3000_rl111612jg.fas
+file=E.coli_K-12_MG1655_GENOME.fa
 
-bowtie-build -f $dr/$file $dr/RL3000
+bowtie-build -f $dr/$file $dr/E.Coli_K-12
