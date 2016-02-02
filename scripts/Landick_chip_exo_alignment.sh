1603945f3288eb3123b3aae8ec446137de8d1a3f
welch16 <welch@stat.wisc.edu>
2016-02-01 18:40:24 -0600

    changed location of script to align to K12 ecoli genome

--- scripts/Landick_chip_exo_alignment.sh
+++ scripts/Landick_chip_exo_alignment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-index=/p/keles/ChIPexo/volume7/Landick/index/RL3000
+index=/p/keles/ChIPexo/volume7/Landick/index/E.Coli_K-12
 mc=24
 
 basedir=/p/keles/ChIPexo
 indir=$basedir/volume8/RawData_from_Biotech_04Dec15
 indir1=$indir/run299
 indir2=$indir/run423
-outdir=$basedir/volume7/Landick/ChIPexo
+outdir=$basedir/volume7/Landick/K12/ChIPexo
 outdir1=$outdir/aerobic_vs_anaerobic
 outdir2=$outdir/rif_treatment
 
@@ -45,3 +45,30 @@ bowtie -q -m 1 -v 2 --best -p $mc -S $index $indir2/run423.edsn-1311-cult-1197-C
 bowtie -q -m 1 -v 2 --best -p $mc -S $index $indir2/run423.edsn-1314-cult-1197-ChIPexo-20minRif-Sigma70_ACAGTG_L001_R1.fastq $outdir2/edsn1314_Sig70.sam
 bowtie -q -m 1 -v 2 --best -p $mc -S $index $indir2/run423.edsn-1317-cult-1202-ChIPexo-0minRif-Sigma70_ACTTGA_L001_R1.fastq $outdir2/edsn1317_Sig70.sam
 bowtie -q -m 1 -v 2 --best -p $mc -S $index $indir2/run423.edsn-1320-cult-1202-ChIPexo-20minRif-Sigma70_GGCTAC_L001_R1.fastq $outdir2/edsn1320_Sig70.sam
+
+
+# Index file: /p/keles/ChIPexo/volume7/Landick/index/E.Coli_K-12 
+# Threads used: 24
+# 1311
+# reads processed: 1050680
+# reads with at least one reported alignment: 902921 (85.94%)
+# reads that failed to align: 89270 (8.50%)
+# reads with alignments suppressed due to -m: 58489 (5.57%)
+# Reported 902921 alignments to 1 output stream(s)
+# 1314
+# reads processed: 2115858
+# reads with at least one reported alignment: 1852124 (87.54%)
+# reads that failed to align: 188420 (8.91%)
+# reads with alignments suppressed due to -m: 75314 (3.56%)
+# Reported 1852124 alignments to 1 output stream(s)
+# 1317
+# reads processed: 2444898
+# reads with at least one reported alignment: 2104427 (86.07%)
+# reads that failed to align: 191414 (7.83%)
+# reads with alignments suppressed due to -m: 149057 (6.10%)
+# reads processed: 13231701                                                                                                                                     
+# 1320                                                               
+# reads with at least one reported alignment: 11548572 (87.28%)                                                                                                                                                                           
+# reads that failed to align: 1131652 (8.55%)   
+# reads with alignments suppressed due to -m: 551477 (4.17%)
+# Reported 11548572 alignments to 1 output stream(s)    
--- scripts/Landick_chip_seq_pet_alignment.sh
+++ scripts/Landick_chip_seq_pet_alignment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-index=/p/keles/ChIPexo/volume7/Landick/index/RL3000
+index=/p/keles/ChIPexo/volume7/Landick/index/E.Coli_K-12
 mc=24
 maxAlign=1
 minins=100
@@ -11,7 +11,7 @@ basedir=/p/keles/ChIPexo
 indir=$basedir/volume8/RawData_from_Biotech_04Dec15
 indir=$indir/run423
 outdir=$basedir/volume7
-outdir=$outdir/Landick/ChIPseq_PET/rif_treatment
+outdir=$outdir/Landick/K12/ChIPseq_PET/rif_treatment
 
 echo Aligning ChIP seq PET files:
 echo Fragment length alowed in the $minins - $maxins range
@@ -57,3 +57,36 @@ bowtie -q -v 2 -a -m 1 -p $mc -S -I $minins -X $maxins $index -1 $indir/run423.e
 
 
 # bowtie -q -v 2 -a -m 1 -p $mc -S $index $indir2/run423.edsn-1320-cult-1202-ChIPexo-20minRif-Sigma70_GGCTAC_L001_R1.fastq $outdir2/edsn1320_Sig70.sam
+
+
+
+# Aligning ChIP seq PET files:
+# Fragment length alowed in the 100 - 500 range
+# Index file: /p/keles/ChIPexo/volume7/Landick/index/E.Coli_K-12
+# Threads used: 24
+# Allowing reads to align to at most 1 positions
+# # reads processed: 11825205
+# # reads with at least one reported alignment: 6722511 (56.85%)
+# # reads that failed to align: 4877126 (41.24%)
+# # reads with alignments suppressed due to -m: 225568 (1.91%)
+# Reported 6722511 paired-end alignments to 1 output stream(s)
+# # reads processed: 14985772
+# # reads with at least one reported alignment: 8269460 (55.18%)
+# # reads that failed to align: 6482333 (43.26%)
+# # reads with alignments suppressed due to -m: 233979 (1.56%)
+# Reported 8269460 paired-end alignments to 1 output stream(s)
+# # reads processed: 9005641
+# # reads with at least one reported alignment: 5821361 (64.64%)
+# # reads that failed to align: 3011056 (33.44%)
+# # reads with alignments suppressed due to -m: 173224 (1.92%)
+# Reported 5821361 paired-end alignments to 1 output stream(s)
+# # reads processed: 14002998
+# # reads with at least one reported alignment: 8427013 (60.18%)
+# # reads that failed to align: 5348751 (38.20%)
+# # reads with alignments suppressed due to -m: 227234 (1.62%)
+# Reported 8427013 paired-end alignments to 1 output stream(s)
+# # reads processed: 16923160
+# # reads with at least one reported alignment: 10374674 (61.30%)
+# # reads that failed to align: 6320137 (37.35%)
+# # reads with alignments suppressed due to -m: 228349 (1.35%)
+# Reported 10374674 paired-end alignments to 1 output stream(s)
