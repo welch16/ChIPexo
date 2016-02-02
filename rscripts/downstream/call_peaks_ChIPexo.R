dd121da8a792e91c9e41a37a123bcb5ebc3d3b82
welch16 <welch@stat.wisc.edu>
2016-02-02 15:08:41 -0600

    changed directories to call peaks for K12 reads alignments

--- rscripts/downstream/call_peaks_ChIPexo.R
+++ rscripts/downstream/call_peaks_ChIPexo.R
@@ -7,24 +7,21 @@ library(parallel)
 
 frag_len <- 150
 bin_size <- 150
-fdr <- .05
 
-out_dir <- "/p/keles/ChIPexo/volume6/resolution/ChIPexo"
-in_dir <- "/p/keles/ChIPexo/volume7/Landick/ChIPexo/rif_treatment"
+in_dir <- "/p/keles/ChIPexo/volume6/K12/downstream/ChIPexo"
 
 files <- c("edsn1311_Sig70.sort.bam","edsn1314_Sig70.sort.bam",
            "edsn1317_Sig70.sort.bam","edsn1320_Sig70.sort.bam")
 
 binfiles <- paste0(files,"_fragL",frag_len,"_bin",bin_size,".txt")
 
-bin_dir <- file.path(out_dir,"bins")
+bin_dir <- file.path(in_dir,"bins")
 
-extra <- file.path("/p/keles/genome_data/EColi_U00096.2",
+extra <- file.path("/p/keles/ChIPexo/volume7/Landick/K12/ChIPexo/peak_inputs",
           c("mappability","GC","N"),
-          "bin",
-          c("fragL150_bin150_32mer_single/E.coli_K-12_MG1655_GENOME_fragL150_bin150.txt",
-            "fragL150_bin150/E.coli_K-12_MG1655_GENOME_GC_fragL150_bin150.txt",
-            "fragL150_bin150/E.coli_K-12_MG1655_GENOME_N_fragL150_bin150.txt"))
+          c("E.coli_K-12_MG1655_GENOME_fragL150_bin150.txt",
+            "E.coli_K-12_MG1655_GENOME_GC_fragL150_bin150.txt",
+            "E.coli_K-12_MG1655_GENOME_N_fragL150_bin150.txt"))
 
 bins <- mclapply(file.path(bin_dir,binfiles),
   function(x)readBins(type = c("chip","M","GC","N"),
@@ -35,17 +32,19 @@ fits <- mclapply(bins,mosaicsFit,mc.cores = 4)
 ## lapply(fits,plot)
 ## dev.off()
 
+fdr <- .05
+peaks <- mclapply(fits,function(x){
+  model <- ifelse(x@bic1S < x@bic2S,"1S","2S")
+  mp <- mosaicsPeak(x,signalModel = model,thres = 100,maxgap = 300)
+  out <- data.table(print(mp))
+  return(out)},mc.cores = 4)
+
 FF <- paste0("FDR",fdr*100)
-out_dir <- file.path(out_dir,"peaks",FF)
+out_dir <- file.path(in_dir,"peaks",FF)
 dir.create(out_dir,recursive = TRUE , showWarnings = FALSE)
 
 peakfiles <- gsub(".sort.bam","_peaks.txt",files)
 
-peaks <- mclapply(fits,function(x){
-  model <- ifelse(x@bic1S < x@bic2S,"1S","2S")
-  mp <- mosaicsPeak(x,signalModel = model,thres = 100)
-  out <- data.table(print(mp))
-  return(out)},mc.cores = 4)
 
 mapply(write.table,peaks,file.path(out_dir,peakfiles),
        MoreArgs = list(quote = FALSE,row.names = FALSE,col.names = FALSE,sep = "\t"))
--- rscripts/downstream/call_peaks_ChIPseq_PET.R
+++ rscripts/downstream/call_peaks_ChIPseq_PET.R
@@ -7,19 +7,17 @@ library(parallel)
 
 frag_len <- 150
 bin_size <- 150
-fdr <- .05
 
-out_dir <- "/p/keles/ChIPexo/volume6/resolution/ChIPseq_PET"
-in_dir <- "/p/keles/ChIPexo/volume7/Landick/ChIPseq_PET/rif_treatment"
+in_dir <- "/p/keles/ChIPexo/volume6/K12/downstream/ChIPseq_PET"
 
 files <- c("edsn1396_Sig70.sort.bam","edsn1398_Sig70.sort.bam",
            "edsn1400_Sig70.sort.bam","edsn1402_Sig70.sort.bam")
 
 binfiles <- paste0(files,"_bin",bin_size,".txt")
 
-bin_dir <- file.path(out_dir,"bins")
+bin_dir <- file.path(in_dir,"bins")
 
-extra <- "/p/keles/ChIPexo/volume6/resolution/inputs/ChIPseq_PET/edsn1369_Input.sort.bam_bin150.txt"
+extra <- "/p/keles/ChIPexo/volume6/K12/downstream/ChIPseq_PET/bins/edsn1369_Input.sort.bam_bin150.txt"
 
 bins <- mclapply(file.path(bin_dir,binfiles),
   function(x)readBins(type = c("chip","input"),
@@ -30,15 +28,16 @@ fits <- mclapply(bins,mosaicsFit,mc.cores = 4)
 ## lapply(fits,plot)
 ## dev.off()
 
+fdr <- .05
 FF <- paste0("FDR",fdr*100)
-out_dir <- file.path(out_dir,"peaks",FF)
+out_dir <- file.path(in_dir,"peaks",FF)
 dir.create(out_dir,recursive = TRUE , showWarnings = FALSE)
 
 peakfiles <- gsub(".sort.bam","_peaks.txt",files)
 
 peaks <- mclapply(fits,function(x){
   model <- ifelse(x@bic1S < x@bic2S,"1S","2S")
-  mp <- mosaicsPeak(x,signalModel = model,thres = 300)
+  mp <- mosaicsPeak(x,signalModel = model,thres = 100,maxgap = 300)
   out <- data.table(print(mp))
   return(out)},mc.cores = 4)
 
