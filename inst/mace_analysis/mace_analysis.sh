87671fba0b2fdaaec233678c86fa10eff19376ae
welch16 <welch@stat.wisc.edu>
2016-02-08 21:00:10 -0600

    changed script to run with new alignment

--- inst/mace_analysis/mace_analysis.sh
+++ inst/mace_analysis/mace_analysis.sh
@@ -2,62 +2,20 @@
 
 ## performs chip exo analysis with MACE.
 
-bamdir=/p/keles/ChIPexo/volume7/Landick/ChIPexo/rif_treatment
+bamdir=/p/keles/ChIPexo/volume7/Landick/K12/ChIPexo/rif_treatment
 ecoli=inst/mace_analysis/ecoli.size
 outdir=inst/mace_analysis
 
+## pre-process
 
-### rif-0min-rep1
+preprocessor.py -i $bamdir/edsn1311_Sig70.sort.bam -r $ecoli -o $outdir/Sig70_rif0_rep1
+preprocessor.py -i $bamdir/edsn1314_Sig70.sort.bam -r $ecoli -o $outdir/Sig70_rif20_rep1
+preprocessor.py -i $bamdir/edsn1317_Sig70.sort.bam -r $ecoli -o $outdir/Sig70_rif0_rep2
+preprocessor.py -i $bamdir/edsn1320_Sig70.sort.bam -r $ecoli -o $outdir/Sig70_rif20_rep2
 
-rep=$bamdir/edsn1311_Sig70.sort.bam
-outfile=$outdir/Sig70_rif0_rep1
+## mace analysis
 
-preprocessor.py -i $rep -r $ecoli -o $outfile
-mace.py -s $ecoli -f $outfile"_Forward.bw" -r $outfile"_Reverse.bw" -o $outfile
-
-### rif-20min-rep1
-
-rep=$bamdir/edsn1314_Sig70.sort.bam
-outfile=$outdir/Sig70_rif20_rep1
-
-preprocessor.py -i $rep -r $ecoli -o $outfile
-mace.py -s $ecoli -f $outfile"_Forward.bw" -r $outfile"_Reverse.bw" -o $outfile
-
-### rif-0min-rep2
-
-rep=$bamdir/edsn1317_Sig70.sort.bam
-outfile=$outdir/Sig70_rif0_rep2
-
-preprocessor.py -i $rep -r $ecoli -o $outfile
-mace.py -s $ecoli -f $outfile"_Forward.bw" -r $outfile"_Reverse.bw" -o $outfile
-
-
-### rif-20min-rep2
-
-rep=$bamdir/edsn1320_Sig70.sort.bam
-outfile=$outdir/Sig70_rif20_rep2
-
-preprocessor.py -i $rep -r $ecoli -o $outfile
-mace.py -s $ecoli -f $outfile"_Forward.bw" -r $outfile"_Reverse.bw" -o $outfile
-
-
-
-# rep1=$bamdir/edsn1311_Sig70.sort.bam
-# rep2=$bamdir/edsn1317_Sig70.sort.bam
-# outfile=Sig70_rif0
-
-# preprocessor.py -i $rep1 -r $ecoli -o $outdir/$outfile
-
-# mace.py -s $ecoli -f $outdir/$outfile"_Forward.bw" -r $outdir/$outfile"_Reverse.bw" -o $outdir/$outfile
-
-# ### rif-20min
-
-# rep1=$bamdir/edsn1314_Sig70.sort.bam
-# rep2=$bamdir/edsn1320_Sig70.sort.bam
-
-# outdir=inst/mace_analysis
-# outfile=Sig70_rif20
-
-# preprocessor.py -i $rep1,$rep2 -r $ecoli -o $outdir/$outfile
-
-# mace.py -s $ecoli -f $outdir/$outfile"_Forward.bw" -r $outdir/$outfile"_Reverse.bw" -o $outdir/$outfile
+mace.py -s $ecoli -f $outdir/Sig70_rif0_rep1_Forward.bw -r $outdir/Sig70_rif0_rep1_Reverse.bw -o $outdir/Sig70_rif0_rep1
+mace.py -s $ecoli -f $outdir/Sig70_rif20_rep1_Forward.bw -r $outdir/Sig70_rif20_rep1_Reverse.bw -o $outdir/Sig70_rif20_rep1
+mace.py -s $ecoli -f $outdir/Sig70_rif0_rep2_Forward.bw -r $outdir/Sig70_rif0_rep2_Reverse.bw -o $outdir/Sig70_rif0_rep2
+mace.py -s $ecoli -f $outdir/Sig70_rif20_rep2_Forward.bw -r $outdir/Sig70_rif20_rep2_Reverse.bw -o $outdir/Sig70_rif20_rep2
