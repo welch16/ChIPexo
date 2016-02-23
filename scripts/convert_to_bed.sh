927656f0be62458c075ebbd91ef4dc8530c3f084
welch16 <welch@stat.wisc.edu>
2016-02-23 12:42:20 -0600

    added edsn931 and edsn933 to convert the bam files to bed

--- scripts/convert_to_bed.sh
+++ scripts/convert_to_bed.sh
@@ -2,11 +2,13 @@
 
 basedir=/p/keles/ChIPexo/volume7/Landick/K12/ChIPexo
 
-indir=$basedir/rif_treatment
-outdir=$basedir/bedfiles
+indir1=$basedir/rif_treatment
+indir2=$basedir/aerobic_vs_anaerobic
+outdir=/p/keles/ChIPexo/volume6/K12/downstream/ChIPexo/bedfiles
 
-bedtools bamtobed -i $indir/edsn1311_Sig70.sort.bam > $outdir/edsn1311_Sig70.sort.bed
-bedtools bamtobed -i $indir/edsn1314_Sig70.sort.bam > $outdir/edsn1314_Sig70.sort.bed
-bedtools bamtobed -i $indir/edsn1317_Sig70.sort.bam > $outdir/edsn1317_Sig70.sort.bed
-bedtools bamtobed -i $indir/edsn1320_Sig70.sort.bam > $outdir/edsn1320_Sig70.sort.bed
-
+bedtools bamtobed -i $indir1/edsn1311_Sig70.sort.bam > $outdir/edsn1311_Sig70.sort.bed
+bedtools bamtobed -i $indir1/edsn1314_Sig70.sort.bam > $outdir/edsn1314_Sig70.sort.bed
+bedtools bamtobed -i $indir1/edsn1317_Sig70.sort.bam > $outdir/edsn1317_Sig70.sort.bed
+bedtools bamtobed -i $indir1/edsn1320_Sig70.sort.bam > $outdir/edsn1320_Sig70.sort.bed
+bedtools bamtobed -i $indir2/edsn931_Sig70.sort.bam > $outdir/edsn931_Sig70.sort.bed
+bedtools bamtobed -i $indir2/edsn933_Sig70.sort.bam > $outdir/edsn933_Sig70.sort.bed
