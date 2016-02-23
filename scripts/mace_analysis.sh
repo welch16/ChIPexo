0479be283eff208503ad329193376be1dcbd366f
welch16 <welch@stat.wisc.edu>
2016-02-23 13:28:22 -0600

    added 933 to mace analysis and changed directories

--- scripts/mace_analysis.sh
+++ scripts/mace_analysis.sh
@@ -2,20 +2,24 @@
 
 ## performs chip exo analysis with MACE.
 
-bamdir=/p/keles/ChIPexo/volume7/Landick/K12/ChIPexo/rif_treatment
-ecoli=inst/mace_analysis/ecoli.size
-outdir=inst/mace_analysis
+bamdir=/p/keles/ChIPexo/volume7/Landick/K12/ChIPexo
+bamdir1=$bamdir/rif_treatment
+bamdir2=$bamdir/aerobic_vs_anaerobic
+outdir=/p/keles/ChIPexo/volume6/K12/other_methods/mace
+ecoli=$outdir/ecoli.size
 
 ## pre-process
 
-preprocessor.py -i $bamdir/edsn1311_Sig70.sort.bam -r $ecoli -o $outdir/Sig70_rif0_rep1
-preprocessor.py -i $bamdir/edsn1314_Sig70.sort.bam -r $ecoli -o $outdir/Sig70_rif20_rep1
-preprocessor.py -i $bamdir/edsn1317_Sig70.sort.bam -r $ecoli -o $outdir/Sig70_rif0_rep2
-preprocessor.py -i $bamdir/edsn1320_Sig70.sort.bam -r $ecoli -o $outdir/Sig70_rif20_rep2
+preprocessor.py -i $bamdir1/edsn1311_Sig70.sort.bam -r $ecoli -o $outdir/edsn1311
+preprocessor.py -i $bamdir1/edsn1314_Sig70.sort.bam -r $ecoli -o $outdir/edsn1314
+preprocessor.py -i $bamdir1/edsn1317_Sig70.sort.bam -r $ecoli -o $outdir/edsn1317
+preprocessor.py -i $bamdir1/edsn1320_Sig70.sort.bam -r $ecoli -o $outdir/edsn1320
+preprocessor.py -i $bamdir2/edsn933_Sig70.sort.bam -r $ecoli -o $outdir/edsn933
 
 ## mace analysis
 
-mace.py -s $ecoli -f $outdir/Sig70_rif0_rep1_Forward.bw -r $outdir/Sig70_rif0_rep1_Reverse.bw -o $outdir/Sig70_rif0_rep1
-mace.py -s $ecoli -f $outdir/Sig70_rif20_rep1_Forward.bw -r $outdir/Sig70_rif20_rep1_Reverse.bw -o $outdir/Sig70_rif20_rep1
-mace.py -s $ecoli -f $outdir/Sig70_rif0_rep2_Forward.bw -r $outdir/Sig70_rif0_rep2_Reverse.bw -o $outdir/Sig70_rif0_rep2
-mace.py -s $ecoli -f $outdir/Sig70_rif20_rep2_Forward.bw -r $outdir/Sig70_rif20_rep2_Reverse.bw -o $outdir/Sig70_rif20_rep2
+mace.py -s $ecoli -f $outdir/edsn1311_Forward.bw -r $outdir/edsn1311_Reverse.bw -o $outdir/edsn1311
+mace.py -s $ecoli -f $outdir/edsn1314_Forward.bw -r $outdir/edsn1314_Reverse.bw -o $outdir/edsn1314
+mace.py -s $ecoli -f $outdir/edsn1317_Forward.bw -r $outdir/edsn1317_Reverse.bw -o $outdir/edsn1317
+mace.py -s $ecoli -f $outdir/edsn1320_Forward.bw -r $outdir/edsn1320_Reverse.bw -o $outdir/edsn1320
+mace.py -s $ecoli -f $outdir/edsn933_Forward.bw -r $outdir/edsn933_Reverse.bw -o $outdir/edsn933
