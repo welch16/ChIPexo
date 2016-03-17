efd989c0dc751b31642a1f05144446be5e6a6986
welch16 <welch@stat.wisc.edu>
2016-03-17 16:23:50 -0500

    added more files from venter and pugh chip exo paper

--- rscripts/download_from_SRA.R
+++ rscripts/download_from_SRA.R
@@ -7,6 +7,11 @@ library(DBI)
 srafile <- getSRAdbFile()
 con <- dbConnect(RSQLite::SQLite(), srafile)
 
+listSRAfile("SRX248184",con)
+listSRAfile("SRX248185",con)
+
+
+
 listSRAfile('SRR770743', con)
 listSRAfile('SRR770744', con)
 listSRAfile('SRA046523',con)
@@ -18,4 +23,5 @@ getSRAfile('SRR770743',con,fileType='sra')
 getSRAfile('SRR770744',con,fileType='sra')
 getSRAfile('SRA046523',con,fileType='sra')
 
-
+getSRAfile("SRX248184",con,fileType = "sra")
+getSRAfile("SRX248185",con,fileType = "sra")
