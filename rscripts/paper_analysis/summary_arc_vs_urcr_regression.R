8d83d67a0ab8cd844193ce97e5bb639c6617402c
welch16 <welch@stat.wisc.edu>
2016-04-12 17:57:03 -0500

    figure 8 on paper

--- rscripts/paper_analysis/summary_arc_vs_urcr_regression.R
+++ rscripts/paper_analysis/summary_arc_vs_urcr_regression.R
@@ -40,27 +40,46 @@ coeffs[ Rep == "Rep-3", expt := gsub("_Rep3","",id)]
 coeffs[grepl("landick_sig70_9",id), expt := "landick_aero"]
 coeffs[grepl("landick_sig70_13",id), expt := "landick_sig70_rif"]
 
+coeffs[,organism := "Human"]
+coeffs[grepl("mouse",id),organism := "Mouse"]
+coeffs[grepl("landick",id),organism := "E.Coli"]
+coeffs[grepl("S2",id),organism := "D.Melanogaster"]
+coeffs[grepl("embryo",id),organism := "D.Melanogaster"]
+
+coeffs[ , seq := "ChIP-exo"]
+coeffs[ grepl("nexus",id), seq := "ChIP-nexus"]
+coeffs <- coeffs[order(seq,organism)]
+
+
 z <- qnorm(.95)
 
 figs_dir <- "figs/test_quantify"
 
 pdf(file = file.path(figs_dir,"arc_urcr_param_regression.pdf"),width = 12,height = 6)
-ggplot(coeffs[term == "b"],aes(Rep , estimate,fill = expt))+geom_bar(stat = "identity")+
-  facet_grid( . ~ expt,scales = "free",space = "free"  )+theme_bw()+
+ggplot(coeffs[term == "b"],aes(id,estimate,fill = organism))+
+  geom_bar(stat = "identity")+
+  theme_bw()+
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 90),
-        strip.text.x = element_text(angle = 70) )+
-  xlab("")+ylab("Intercept")
-ggplot(coeffs[term == "k"],aes(Rep ,estimate,fill = expt))+geom_bar(stat = "identity")+
-  facet_grid( . ~ expt,scales = "free",space = "free"  )+theme_bw()+
+        strip.text.x = element_text(angle = 0),
+        strip.background = element_blank(),
+        plot.title = element_text(hjust = 0))+
+  facet_grid( ~ seq + organism  ,scales = "free",space = "free")+
+  ylab("Intercept")+xlab("Experiment")+
+  scale_fill_brewer(palette = "Set1")+ggtitle("B")
+ggplot(coeffs[term == "k"],aes(id,estimate,fill = organism))+
+  geom_bar(stat = "identity")+
+  theme_bw()+
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 90),
-        strip.text.x = element_text(angle = 70) )+
-  xlab("")+ylab("Slope")
-ggplot(coeffs[term == "k"],aes(Rep ,1 / estimate,fill = expt))+geom_bar(stat = "identity")+
-  facet_grid( . ~ expt,scales = "free",space = "free"  )+theme_bw()+
-  theme(legend.position = "none",
-        axis.text.x = element_text(angle = 90),
-        strip.text.x = element_text(angle = 70) )+
-  xlab("")+ylab("1  / Slope")
+        strip.text.x = element_text(angle = 0),
+        strip.background = element_blank(),
+        plot.title = element_text(hjust = 0))+
+  facet_grid( ~ seq + organism  ,scales = "free",space = "free")+
+  ylab("Slope")+xlab("Experiment")+
+  scale_fill_brewer(palette = "Set1")+ggtitle("C")
 dev.off()
+
+  ## geom_text(data=coeffs[term == "b"],aes(id,estimate,label=round(estimate,2)),vjust=1,hjust = .5,angle = 90) +
+
+  ## geom_text(data=coeffs[term == "k"],aes(id,estimate,label=round(estimate,2)),vjust=1,hjust = .5,angle = 90) +
--- rscripts/paper_analysis/summary_npos_depth_regression.R
+++ rscripts/paper_analysis/summary_npos_depth_regression.R
@@ -31,17 +31,20 @@ dt <- data.table(id = sub(".RData","",files),
                  m = mean_est,var = var_est)
 dt[,lb := m - z * sqrt(var) / N]
 dt[,ub := m + z * sqrt(var) / N]
-dt[,organism := "human"]
-dt[grepl("mouse",id),organism := "mouse"]
-dt[grepl("landick",id),organism := "e.coli"]
-dt[grepl("S2",id),organism := "drosophila"]
-dt[grepl("embryo",id),organism := "drosophila"]
+dt[,organism := "Human"]
+dt[grepl("mouse",id),organism := "Mouse"]
+dt[grepl("landick",id),organism := "E.Coli"]
+dt[grepl("S2",id),organism := "D.melanogaster"]
+dt[grepl("embryo",id),organism := "D.melanogaster"]
 
-DT[,organism := "human"]
-DT[grepl("mouse",id),organism := "mouse"]
-DT[grepl("landick",id),organism := "e.coli"]
-DT[grepl("S2",id),organism := "drosophila"]
-DT[grepl("embryo",id),organism := "drosophila"]
+DT[,organism := "Human"]
+DT[grepl("mouse",id),organism := "Mouse"]
+DT[grepl("landick",id),organism := "E.Coli"]
+DT[grepl("S2",id),organism := "D.Melanogaster"]
+DT[grepl("embryo",id),organism := "D.Melanogaster"]
+
+
+
 
 
 figs_dir <- "figs/test_quantify"
@@ -78,9 +81,27 @@ dev.off()
 
   ## 
 
+DT[ , seq := "ChIP-exo"]
+DT[ grepl("nexus",id), seq := "ChIP-nexus"]
+
+
+DT <- DT[order(seq,organism)]
+
 
 figs_dir <- "figs/test_quantify"
 pdf(file = file.path(figs_dir,"slope_npos_depth_boxplot.pdf"),width = 12,height = 6)
+ggplot(DT,aes(id,estimate,fill = organism , colour = organism))+
+  geom_boxplot(outlier.size = 0)+
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 90),
+        strip.text.x = element_text(angle = 0),
+        strip.background = element_blank(),
+        plot.title = element_text(hjust= 0))+
+  facet_grid( ~ seq + organism  ,scales = "free",space = "free")+xlab("")+
+  xlab("Experiment")+ylab("Ratio between depth and nr. unique positions change")+
+  scale_y_log10()+scale_fill_brewer(palette = "Pastel1")+scale_color_brewer(palette = "Set1")+
+  geom_abline(slope = 0, intercept = 1,linetype = 2,colour = "darkgrey")+ggtitle("A")
 ggplot(DT,aes(id,estimate))+geom_boxplot(outlier.size = 0)+
   theme(legend.position = "none",axis.text.x = element_text(angle = 90))+
   facet_grid( ~ organism  ,scales = "free",space = "free")+xlab("")+
