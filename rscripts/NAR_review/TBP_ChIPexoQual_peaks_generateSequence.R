2c64c7f15f016fde2cb6b1c4495a68f0540dd43d
welch16 <welch@stat.wisc.edu>
2017-01-09 20:24:46 -0600

    scripts to generate sequences from ChIP-exo/nexus peaks

---
+++ rscripts/NAR_review/TBP_ChIPexoQual_peaks_generateSequence.R
@@ -0,0 +1,95 @@
+
+rm(list = ls())
+
+library(ChIPexoQual)
+library(magrittr)
+
+dr = "/p/keles/ChIPexo/volume4"
+
+files = list.files(dr,recursive = TRUE,full.names = TRUE)
+files = files[grep("carroll",files)]
+files = files[grep("bam",files)]
+files = files[grep("mouse",files)]
+files = files[grep("sort",files)]
+files = files[grep("bai",files,invert = TRUE)]
+files = files[grep("txt",files,invert = TRUE)]
+
+library(parallel)
+
+options("mc.cores" = 22)
+
+reads = files %>% mclapply(readGAlignments,param = NULL)
+reads = reads %>% mclapply(as,"GRanges")
+names(reads) = paste0("Rep",c(3,1,2))
+
+exo = lapply(reads,function(x)ExoData(reads =  x))
+
+peakfiles = list.files(dr,full.names = TRUE,recursive = TRUE,pattern = "peaks")
+peakfiles = peakfiles[grep("mouse",peakfiles)]
+
+library(readr)
+library(dplyr)
+library(data.table)
+
+peaks = mclapply(peakfiles,read_delim,delim = " ",col_names = FALSE)
+peaks = lapply(peaks,function(x){
+    x = x %>% select(X1,X2,X3)
+    setnames(x,names(x),c("seqnames","start","end"))
+    x = as.data.table(x)
+  return(ChIPUtils::dt2gr(x))})
+names(peaks) = paste0("Rep",c(3,1,2))
+
+## peak columns:
+## chrID peakStart peakStop peakSize logAveP logMinP aveLogP aveChipCount maxChipCount map GC 
+
+## join all peak regions together
+all_peaks = Reduce(c,peaks) %>% reduce
+
+exo_peaks = mclapply(exo,subsetByOverlaps,all_peaks)
+readlength = reads %>% sapply(function(x)x %>% width %>% median)
+
+## common sense filter
+
+## remove chrM
+exo_peaks = exo_peaks %>% mclapply(function(x)x[as.character(seqnames(x)) != "chrM"])
+
+## width analysis
+
+exo_peaks %>% lapply(function(x)width(x) %>% summary)
+
+## $Rep3
+##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+##   34.00   36.00   53.00   80.97   98.00 3399.00 
+
+## $Rep1
+##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+##    35.0    60.0   233.0   254.8   400.0  3390.0 
+
+## $Rep2
+##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+##    35.0    39.0    86.0   147.8   234.0  3388.0 
+
+rl = median(readlength)
+
+K = 3
+exo_peaks = exo_peaks %>% lapply(function(x)x[width(x) >= K * rl])
+
+library(BSgenome.Mmusculus.UCSC.mm9)
+
+sequences = mclapply(exo_peaks,function(x)
+   getSeq(Mmusculus,x),mc.cores = 3)
+
+sequences = mclapply(sequences,function(x)as.character(x),mc.cores =3)
+
+nms = lapply(exo_peaks,function(x){
+  paste0(as.character(seqnames(x)),":",start(x),"-",end(x))})
+
+fasta_formats = mapply(function(nms,seqs)paste0(">",nms,"\n",seqs),
+                        nms,sequences,SIMPLIFY = FALSE)
+
+exo_dir <- "/p/keles/ChIPexo/volume4/carroll_data/mouse/fasta_format_exo"
+
+mapply(write.table,fasta_formats,
+       file.path(exo_dir,"sequences",
+                 gsub("sort.bam","_exo_peak_sequences.fna",basename(files))),
+  MoreArgs = list(quote = FALSE,row.names = FALSE,col.names = FALSE))
